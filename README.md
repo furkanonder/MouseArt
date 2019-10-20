73c983829a8f991088862baa98e2d69563fb4500
furkanonder <furkantahaonder@gmail.com>
2019-10-21 00:19:53 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## MouseArt
+A simple program that draws mouse movements.
+
+## Installation
+Install the requirements:
+ ```python
+ pip install -r requirements.txt
+```
+
+## Running
+```python
+ python mouse_art.py
+```
---
+++ mouse_art.py
@@ -0,0 +1,38 @@
+import time
+import tkinter as tk
+import pyautogui
+
+
+class Root(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.canvas = tk.Canvas(self, width=1200, height=700)
+        self.exit_button = tk.Button(self, text="Quit", command=self.destroy)
+        self.exit_button.pack()
+        self.canvas.old_coords = None
+        self.canvas.pack()
+        self.bind_mouse()
+
+    def draw(self, x, y):
+        if self.canvas.old_coords:
+            x1, y1 = self.canvas.old_coords
+            self.canvas.create_line(x, y, x1, y1)
+        self.canvas.old_coords = x, y
+        self.canvas.update()
+
+    def bind_mouse(self):
+        old_x = old_y = 0
+        try:
+            while True:
+                x, y = pyautogui.position()
+                if old_x != x and old_y != y:
+                    self.draw(x, y)
+                    old_x, old_y = x, y
+                time.sleep(0.01)
+        except KeyboardInterrupt:
+            pass
+
+
+if __name__ == "__main__":
+    root = Root()
+    root.mainloop()
