16341444eeb7ad08e300adbf2165f5ee1db2ecb8
furkanonder <furkantahaonder@gmail.com>
2019-10-22 23:53:46 +0300

    add create_canvas and quit functions

--- mouse_art.py
+++ mouse_art.py
@@ -1,17 +1,26 @@
+import re
 import time
 import tkinter as tk
+
 import pyautogui
 
 
 class Root(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.canvas = tk.Canvas(self, width=1200, height=700)
-        self.exit_button = tk.Button(self, text="Quit", command=self.destroy)
+        self.title("MouseArt")
+        self.create_canvas()
+        self.exit_button = tk.Button(self, text="Quit", command=self.quit)
         self.exit_button.pack()
         self.canvas.old_coords = None
         self.canvas.pack()
+        self.quit_flag = 1
         self.bind_mouse()
+
+    def create_canvas(self):
+        self.size = str(pyautogui.size())
+        self.width, self.height = re.findall(r"\d+", self.size)
+        self.canvas = tk.Canvas(self, width=self.width, height=self.height)
 
     def draw(self, x, y):
         if self.canvas.old_coords:
@@ -22,15 +31,17 @@ class Root(tk.Tk):
 
     def bind_mouse(self):
         old_x = old_y = 0
-        try:
-            while True:
-                x, y = pyautogui.position()
-                if old_x != x and old_y != y:
-                    self.draw(x, y)
-                    old_x, old_y = x, y
+        while self.quit_flag:
+            x, y = pyautogui.position()
+            y -= 20
+            if old_x != x and old_y != y:
+                self.draw(x, y)
+                old_x, old_y = x, y
                 time.sleep(0.01)
-        except KeyboardInterrupt:
-            pass
+
+    def quit(self):
+        self.quit_flag = 0
+        self.destroy()
 
 
 if __name__ == "__main__":
